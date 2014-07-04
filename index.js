c46d63d2fd63c0788f112037dbd49d3b10cc8f44
Sumit Chawla <sumitkchawla@gmail.com>
2014-07-04 14:20:38 -0700

    Use require.resolve

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var scope = require('jsdom').jsdom().createWindow();
-var websocket_multiplex = require('websocket-multiplex');
+var websocket_multiplex = require.resolve('websocket-multiplex');
 var fs = require('fs');
 var path = require('path');
 
-var library = path.join(__dirname, 'node_modules/websocket-multiplex/multiplex_client.js');
+var library = path.join(path.dirname(websocket_multiplex),'multiplex_client.js');
 
 scope.eval(fs.readFileSync(library, 'utf-8'));
 module.exports = scope.WebSocketMultiplex;
